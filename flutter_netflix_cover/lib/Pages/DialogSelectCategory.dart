cd4962d30206b4539384173549a9cf8b9beb6e65
Abe Wang <abewang@kkstream.com>
2021-09-14 18:24:31 +0800

    feat: add dialog page.

--- flutter_netflix_cover/lib/Pages/DialogSelectCategory.dart
+++ flutter_netflix_cover/lib/Pages/DialogSelectCategory.dart
@@ -8,8 +8,55 @@ class DialogSelectCategory extends StatefulWidget {
 }
 
 class _DialogSelectCategoryState extends State<DialogSelectCategory> {
+  List<String> _list = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "All"];
+  String selected = "All";
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 64.0),
+            child: ListView(
+              shrinkWrap: true,
+              children: List.generate(_list.length, (index) => _buildTitle(_list[index])),
+            ),
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.of(context).pop();
+              },
+              child: Icon(
+                Icons.cancel,
+                size: 64.0,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buildTitle(String data) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Text(
+          data,
+          style: TextStyle(
+            fontSize: selected == data ? 24.0: 20.0,
+            color: selected == data ? Colors.white: Colors.white.withOpacity(0.6)
+          ),
+        ),
+      ),
+    );
   }
 }
