a6195f849ce0daeca88c08c5944047dd6f95b6a7
Abe Wang <abewang@kkstream.com>
2021-09-19 00:37:07 +0800

    chore: update more page.

--- flutter_netflix_cover/lib/Pages/MorePage.dart
+++ flutter_netflix_cover/lib/Pages/MorePage.dart
@@ -57,12 +57,12 @@ class _MorePageState extends State<MorePage> {
         children: [
           Row(
             children: [
-              Icon(Icons.message, color: Colors.white,),
-              Text("將 Flutter 介紹給好友", style: TextStyle(color: Colors.white),)
+              Icon(Icons.message, color: Colors.white),
+              Text("將 Flutter 介紹給好友", style: TextStyle(color: Colors.white))
             ],
           ),
-          Text("將此連結分享給朋友，讓他們也可以一起討論。", style: TextStyle(color: Colors.white),),
-          Text("條款與條件", style: TextStyle(color: Colors.white),),
+          Text("將此連結分享給朋友，讓他們也可以一起討論。", style: TextStyle(color: Colors.white)),
+          Text("條款與條件", style: TextStyle(color: Colors.indigo)),
           Container(
             color: Colors.black,
             child: Row(
@@ -84,11 +84,49 @@ class _MorePageState extends State<MorePage> {
           Row(
             mainAxisSize: MainAxisSize.max,
             children: [
-
+              _buildShareIcon("Line", "assets/book.png"),
+              _buildDivider(),
+              _buildShareIcon("Facebook", "assets/book.png"),
+              _buildDivider(),
+              _buildShareIcon("Gmail", "assets/book.png"),
+              _buildDivider(),
+              Expanded(
+                child: Column(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.all(4.0),
+                      width: 30.0,
+                      height: 30.0,
+                      child: Icon(Icons.more_horiz, color: Colors.white),
+                    ),
+                    Text("More", style: TextStyle(color: Colors.white))
+                  ],
+                ),
+              )
             ],
           )
         ],
       ),
+    );
+  }
+
+  Widget _buildShareIcon(String title, String assetName) {
+    return Expanded(
+      child: Column(
+        children: [
+          Padding(padding: const EdgeInsets.all(4.0),
+              child: Image.asset(assetName, width: 30.0, height: 30.0)),
+          Text(title, style: TextStyle(color: Colors.white),)
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDivider() {
+    return Container(
+      color: Colors.grey.withOpacity(0.6),
+      width: 2.0,
+      height: 40.0,
     );
   }
 
